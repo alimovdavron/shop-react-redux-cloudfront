f7cadcbb9a9d4c3657844973d67b6b7d50fe2c61
Alimov Davron <alimov.davron.21@gmail.com>
2021-09-03 16:25:09 +0500

    feat: update cart api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://fw3b67j1u8.execute-api.eu-central-1.amazonaws.com/dev',
   import: 'https://jra3ihgafh.execute-api.eu-central-1.amazonaws.com/dev',
   bff: 'https://fw3b67j1u8.execute-api.eu-central-1.amazonaws.com/dev',
-  cart: 'https://fw3b67j1u8.execute-api.eu-central-1.amazonaws.com/dev',
+  cart: 'http://alimovdavron-cart-api-develop.eu-central-1.elasticbeanstalk.com',
 };
 
 export default API_PATHS;
